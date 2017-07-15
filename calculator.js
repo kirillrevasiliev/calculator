206ffe4697ece4b7486f140a0f4ba003d3b7d44f
Kirill Vasiliev <planetastock@gmail.com>
2017-07-15 21:54:55 +0300

    wrote the function parse string, connected with html

--- calculator.js
+++ calculator.js
@@ -1,54 +1,82 @@
-//
+(function(){
+
+    // змінна для запису тимчасових результатів
+    val: 0;
+    // змінна для виводу помилки
+    myerror : "Ви ввели неправильні аргументи";
+
+    // для перевірки змінної на число використаємо
+    // написану раніше функцію isNumeric()
+    function isNumeric(x){
+        return !isNaN(parseFloat(x)) && isFinite(x);
+    };
+    // напишемо ф-цію для перевірки двох змінних на число,
+    // якщо функція повертає true - значить обидві змінні це число
+    function isNumForTwo(x, y){
+        if((isNumeric(x) && isNumeric(x)) && (isNumeric(y) && isNumeric(y))){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    //
+    function getfunc(x, y, a){
+        var val = 0;
+        switch (a){
+            case '+': val = x + y;
+                return val;
+                break;
+            case "-": val = x - y;
+                return val;
+                break;
+            case "*": val = x * y;
+                return val;
+                break;
+            case "/": val = x / y;
+                return val;
+                break;
+            default: myerror;
+                break;
+        }
+    };
+
+
 var calculator = {
 
-    val: 0,
     getsum: function (x, y) {
-        if(!(this.isNumeric(x) && this.isNumeric(y))) return console.log("Введіть числа");
-            return this.val =x + y;
+        if(!isNumForTwo(x, y)) return myerror;
+        return val = x + y;
         },
     getdif: function (x, y) {
-        if(!(this.isNumeric(x) && this.isNumeric(y))) return console.log("Введіть числа");
-            return this.val = x - y;
+        if(!isNumForTwo(x, y)) return myerror;
+        return val = x - y;
     },
     getmul: function (x, y) {
-        if(!(this.isNumeric(x) && this.isNumeric(y))) return console.log("Введіть числа");
-            return this.val = x * y;
+        if(!isNumForTwo(x, y)) return myerror;
+            return val = x * y;
     },
     getdiv: function (x, y) {
-        if(!(this.isNumeric(x) && this.isNumeric(y))) return console.log("Введіть числа");
-            return this.val = x / y;
+        if(!isNumForTwo(x, y)) return myerror;
+        return val = x / y;
     },
 
-    getfunc: function(x, y, a){
-        switch (a){
-            case '+': this.getsum(x, y);
-                break;
-            case "-": this.getdif(x, y);
-                break;
-            case "*": this.getmul(x, y);
-                break;
-            case "/": this.getdiv(x, y);
-                break;
-            default: console.log("нипанятно!!");
-            break;
-        }
-    },
     // функція для визначення степеню "z" числа "x"
     powloop: function(x, z){
-        if(!(this.isNumeric(x) && this.isNumeric(z))) return console.log("Введіть числа");
+        if(!(isNumForTwo(x, z))) return console.log("Введіть числа");
         var i;
         var res = x;
         for(i = 1; i < z; i++){
             res = this.getmul(res, x);
         }
-        return this.val = res;
+        return val = res;
     },
     // рекурсивная функція
     // переписано після прочитанної статі
     // про рекурсивні функції та їх контекст
     powrecurs: function(x, z){
 
-        if(!(this.isNumeric(x) && this.isNumeric(z))) return console.log("Введіть числа");
+        if(!(isNumeric(x) && isNumeric(z))) return console.log("Введіть числа");
 
         if(z != 1){
             // виклик ф-ції в рекурсії
@@ -57,15 +85,111 @@ var calculator = {
             return this.val = x;
         }
     },
-
-    // для перевірки змінної на число використаємо
-    // написану раніше функцію isNumeric()
     isNumeric: function(x){
         return !isNaN(parseFloat(x)) && isFinite(x);
     }
+};
+    window.calc = calculator;
+})();
 
+
+// //for(var i = 0; i < str.length; i++){
+// console.log(str.charAt(i));
+// }
+
+// ------------
+/*
+var func = function(){
+    var a = 10;
+    return function () {
+        a++;
+    }
 };
 
+func();
+alert(a);
 
+(function (){
+    var a;
+    function b(){
+        a++;
+    }
+    window.b();
+})*/
 
-//
+function parsestr(str){
+    // визначаємо довжину рядка
+    var lenght = str.length;
+    // ітератор
+    var i = 0;
+    // змінна для першого аргументу
+    var first = "";
+    // змінна для другого елементу
+    var second = "";
+    // змінна для символу оператору
+    var operator;
+    // поки ітератор менший або дорівнює
+    while(i < lenght){
+        // якщо символ цифра
+        if(isNumeric(str[i])){
+            debugger;
+            // записуємо символ в строку
+            first += str[i];
+            //перевіряємо чи наступний символ цифра
+            if(isNumeric(str[i+1])){
+                debugger;
+                // якщо так продовжуємо цикл
+                i++;
+                continue;
+            }
+            i++;
+        // якцщо сивол не цифра. записуємо його в змінну оператору
+        } else {
+            operator = str[i];
+            debugger;
+            // якщо наступний символ цифра
+            if(isNumeric(str[i+1])){
+                i++; // піднімаємо ітератор на одиницю
+                // поки наступний символ цифра
+                while(isNumeric(str[i])){
+                    // добавляємо його в змінну для другого аргументу
+                    second += str[i];
+                    // піднімаємо ітератор на одиницю
+                    i++;
+                    debugger;
+                }
+                // проводимо підрахунок і записуємо результат в першу змінну
+                first = getfunc(+first, +second, operator);
+                // обнуляємо другу змінну
+                second = "";
+                debugger;
+            }
+        }
+    }
+    // повертаємо результат
+    return first;
+};
+
+function isNumeric(x){
+    return !isNaN(parseFloat(x)) && isFinite(x);
+};
+
+function getfunc(x, y, a){
+    var val = 0;
+    switch (a){
+        case '+': val = x + y;
+            return val;
+            break;
+        case "-": val = x - y;
+            return val;
+            break;
+        case "*": val = x * y;
+            return val;
+            break;
+        case "/": val = x / y;
+            return val;
+            break;
+        default: myerror;
+            break;
+    }
+};
